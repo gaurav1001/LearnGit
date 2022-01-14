653450ee6591abddad80f5ddeeab3093fd0d10a3
Rtrcreator <Rtrcreator@gmail.com>
2022-01-14 13:09:34 +0530

    further commit

--- app/src/main/java/com/example/learngit/MainActivity.kt
+++ app/src/main/java/com/example/learngit/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.learngit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.v(Tag,"Hello World")
+        val msg = findViewById<TextView>(R.id.msg)
+        msg.text = "Latest change"
     }
     companion object{
         const val Tag = "TAG"
